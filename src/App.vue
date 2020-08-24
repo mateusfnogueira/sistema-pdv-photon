bc0fe782c2ef711d01b661081e19e0d126fe4142
Mateus <mateusfnogueira@outlook.com>
2020-08-24 16:04:30 -0300

    commit on sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from "./components/SideBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SideBar,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="window">
+    <div class="window-content">
+      <div class="pane-group">
+        <div class="pane-mini sidebar">...</div>
+        <div class="pane">...</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
